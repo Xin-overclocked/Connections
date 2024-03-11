c7707368b4e1a92dc6946b8d782c29c84aae1dd7
ChuaPeiJun <chua17009@gmail.com>
2024-03-11 17:20:48 +0800

    login, search, messages, profile page added

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,111 @@ class MyApp extends StatelessWidget {
       title: 'Mentor-Mentee',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        fontFamily: 'Roboto', // Change the font to Roboto for consistency
       ),
-      home: MyHomePage(),
+      home: LoginPage(), // Set the initial route to the LoginPage
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.blue.shade400, Colors.blue.shade900],
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(36.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 30.0),
+                  TextField(
+                    controller: _usernameController,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      hintText: 'Username',
+                      hintStyle: TextStyle(color: Colors.grey),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide.none,
+                      ),
+                      prefixIcon: Icon(Icons.person, color: Colors.grey),
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  TextField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      hintText: 'Password',
+                      hintStyle: TextStyle(color: Colors.grey),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide.none,
+                      ),
+                      prefixIcon: Icon(Icons.lock, color: Colors.grey),
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Implement your login logic here
+                      // For simplicity, let's just navigate to the main page
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => MyHomePage()),
+                      );
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                      ),
+                    ),
+                    child: Ink(
+                      padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
@@ -21,6 +124,9 @@ class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
+
+
+
 
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
@@ -32,7 +138,7 @@ class _MyHomePageState extends State<MyHomePage> {
     MessagesWidget(),
   ];
 
-  List<String> _appBarTitles = ['Home', 'Search', 'Contact', 'Messages'];
+  List<String> _appBarTitles = ['Home', 'Find Friends & Events', 'Contact', 'Messages'];
 
   void _onItemTapped(int index) {
     setState(() {
@@ -44,7 +150,12 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(_appBarTitles[_selectedIndex]),
+        title: Text(
+          _appBarTitles[_selectedIndex],
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         actions: [
           IconButton(
             icon: Icon(Icons.notifications),
@@ -66,9 +177,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.black,
         items: <BottomNavigationBarItem>[
@@ -110,15 +219,59 @@ class HomeWidget extends StatelessWidget {
   }
 }
 
-class SearchWidget extends StatelessWidget {
+class SearchWidget extends StatefulWidget {
+  @override
+  State<SearchWidget> createState() => _SearchWidgetState();
+}
+
+class _SearchWidgetState extends State<SearchWidget> {
+  TextEditingController _searchController = TextEditingController();
+  List<Person> _searchResults = [];
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Search',
-        style: TextStyle(fontSize: 24),
-      ),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            controller: _searchController,
+            onChanged: _search,
+            decoration: InputDecoration(
+              labelText: 'Search',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              prefixIcon: Icon(Icons.search),
+            ),
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: _searchResults.length,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                title: Text(_searchResults[index].name),
+                onTap: () {
+                  // Implement what happens when a user taps on a search result
+                },
+              );
+            },
+          ),
+        ),
+      ],
     );
+  }
+
+  void _search(String query) {
+    setState(() {
+      _searchResults.clear();
+      for (Person person in people) {
+        if (person.name.toLowerCase().contains(query.toLowerCase())) {
+          _searchResults.add(person);
+        }
+      }
+    });
   }
 }
 
@@ -140,21 +293,33 @@ class MessagesWidget extends StatelessWidget {
     return ListView.builder(
       itemCount: people.length,
       itemBuilder: (BuildContext context, int index) {
-        return InkWell(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => ChatScreen(people[index])),
-            );
-          },
-          child: ListTile(
-            leading: CircleAvatar(
-              child: Text(people[index].name[0]),
+        // Check if the person is not "Universiti Sains Malaysia"
+        if (people[index].name != 'Universiti Sains Malaysia') {
+          return InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ChatScreen(people[index])),
+              );
+            },
+            child: ListTile(
+              leading: CircleAvatar(
+                child: Text(people[index].name[0]),
+              ),
+              title: Text(
+                people[index].name,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                'Last message: ${people[index].messages.last.text}',
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
             ),
-            title: Text(people[index].name),
-            subtitle: Text('Last message: ${people[index].messages.last.text}'),
-          ),
-        );
+          );
+        } else {
+          return SizedBox.shrink(); // Return an empty widget if it's "Universiti Sains Malaysia"
+        }
       },
     );
   }
@@ -163,36 +328,57 @@ class MessagesWidget extends StatelessWidget {
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    String userName = 'Low Jia Xin'; // Example user name
+    String firstLetter = userName.isNotEmpty ? userName[0] : ''; // Extract first letter of the user's name
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile'),
+        title: Text(
+          'Profile',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => LoginPage()),
+              );
+            },
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start, // Align children to the start
           children: [
             CircleAvatar(
               radius: 50,
               backgroundColor: Colors.blue,
-              child: Icon(Icons.account_circle, size: 80, color: Colors.white),
+              child: Text(
+                firstLetter.toUpperCase(), // Display first letter as uppercase
+                style: TextStyle(fontSize: 40, color: Colors.white), // Adjust size and color as needed
+              ),
             ),
             SizedBox(width: 20),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  'John Doe',
+                  userName,
                   style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                 ),
                 SizedBox(height: 10),
                 Text(
-                  'Username: johndoe123',
+                  'Username: jiaxin123',
                   style: TextStyle(fontSize: 16, color: Colors.grey),
                 ),
                 SizedBox(height: 10),
                 Text(
-                  'Email: john.doe@example.com',
+                  'Email: jiaxin@example.com',
                   style: TextStyle(fontSize: 16, color: Colors.grey),
                 ),
                 SizedBox(height: 20),
@@ -201,6 +387,20 @@ class ProfilePage extends StatelessWidget {
                     // Add functionality for editing profile
                   },
                   child: Text('Edit Profile'),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    // Add functionality for editing profile
+                  },
+                  child: Text('Add Bio'),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    // Add functionality for editing profile
+                  },
+                  child: Text('Add Hashtag'),
                 ),
               ],
             ),
@@ -211,15 +411,26 @@ class ProfilePage extends StatelessWidget {
   }
 }
 
+
 class NotificationsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Notifications'),
+        title: Text(
+          'Notifications',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
       body: Center(
-        child: Text('No Notification'),
+        child: Text('No Notification',
+        style: 
+          TextStyle(
+            fontSize: 20
+          ),
+        ),
       ),
     );
   }
@@ -241,7 +452,12 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.person.name),
+        title: Text(
+          widget.person.name,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
       body: Column(
         children: <Widget>[
@@ -346,6 +562,12 @@ List<Person> people = [
       Message('Hello!', false),
       Message('I\'m doing great, thanks!', false),
       Message('How about you?', true),
+    ],
+  ),
+  Person(
+    'Universiti Sains Malaysia',
+    [
+     
     ],
   ),
 ];
