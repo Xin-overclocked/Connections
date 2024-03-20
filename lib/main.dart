7160990f2873cf82c78e800c35ff8f289aac77b7
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 <147032990+Xin-overclocked@users.noreply.github.com>
2024-03-20 19:20:17 +0800

    pub get dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -150,7 +150,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   List<String> _appBarTitles = [
     'mentor-mentee',
-    'find friends & events',
+    'find connections & events',
     'make connections',
     'messages'
   ];
@@ -225,14 +225,9 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 class EnrollButton extends StatefulWidget {
-  // final bool enrolled; // Track enrollment state
-
-  // const EnrollButton({
-  //   required this.enrolled,
-  // });
-
+  const EnrollButton({Key? key}) : super(key: key);
   @override
-  _EnrollButtonState createState() => _EnrollButtonState();
+  State<EnrollButton> createState() => _EnrollButtonState();
 }
 
 class _EnrollButtonState extends State<EnrollButton> {
@@ -557,6 +552,114 @@ class SimpleProfilePage extends StatelessWidget {
                 style: TextStyle(fontSize: 16, color: Colors.grey),
               ),
             ),
+            Wrap(
+              spacing: 4.0, // gap between lines
+              children: <Widget>[
+                Chip(
+                  avatar: CircleAvatar(
+                      backgroundColor: Colors.orange,
+                      child: Text('C', style: TextStyle(color: Colors.white))),
+                  label: Text('Cupcake'),
+                  backgroundColor: Colors.white,
+                ),
+                Chip(
+                  avatar: CircleAvatar(
+                      backgroundColor: Colors.cyanAccent,
+                      child:
+                          Text('D', style: TextStyle(color: Colors.black45))),
+                  label: Text('Donut'),
+                  backgroundColor: Colors.white,
+                ),
+                Chip(
+                  avatar: CircleAvatar(
+                      backgroundColor: Colors.indigoAccent,
+                      child: Text('E', style: TextStyle(color: Colors.white))),
+                  label: Text('Eclair'),
+                  backgroundColor: Colors.white,
+                ),
+                Chip(
+                  avatar: CircleAvatar(
+                      backgroundColor: Colors.yellowAccent,
+                      child:
+                          Text('F', style: TextStyle(color: Colors.black45))),
+                  label: Text('Froyo'),
+                  backgroundColor: Colors.white,
+                ),
+                Chip(
+                  avatar: CircleAvatar(
+                      backgroundColor: Colors.green,
+                      child: Text('G', style: TextStyle(color: Colors.white))),
+                  label: Text('Gingerbread'),
+                  backgroundColor: Colors.white,
+                ),
+                Chip(
+                  avatar: CircleAvatar(
+                      backgroundColor: Colors.redAccent,
+                      child: Text('H', style: TextStyle(color: Colors.white))),
+                  label: Text('Honeycomb'),
+                  backgroundColor: Colors.white,
+                ),
+                Chip(
+                  avatar: CircleAvatar(
+                      backgroundColor: Colors.greenAccent,
+                      child:
+                          Text('I', style: TextStyle(color: Colors.black45))),
+                  label: Text('Ice cream Sandwich'),
+                  backgroundColor: Colors.white,
+                ),
+                Chip(
+                  avatar: CircleAvatar(
+                      backgroundColor: Colors.deepOrangeAccent,
+                      child: Text('J', style: TextStyle(color: Colors.white))),
+                  label: Text('Jelly Bean'),
+                  backgroundColor: Colors.white,
+                ),
+                Chip(
+                  avatar: CircleAvatar(
+                      backgroundColor: Colors.indigo,
+                      child: Text('K', style: TextStyle(color: Colors.white))),
+                  label: Text('Kit Kat'),
+                  backgroundColor: Colors.white,
+                ),
+                Chip(
+                  avatar: CircleAvatar(
+                      backgroundColor: Colors.tealAccent,
+                      child:
+                          Text('L', style: TextStyle(color: Colors.black45))),
+                  label: Text('Lollipop'),
+                  backgroundColor: Colors.white,
+                ),
+                Chip(
+                  avatar: CircleAvatar(
+                      backgroundColor: Colors.amberAccent,
+                      child: Text('M', style: TextStyle(color: Colors.white))),
+                  label: Text('Marshmallow'),
+                  backgroundColor: Colors.white,
+                ),
+                Chip(
+                  avatar: CircleAvatar(
+                      backgroundColor: Colors.cyan,
+                      child: Text('N', style: TextStyle(color: Colors.white))),
+                  label: Text('Nougat'),
+                  backgroundColor: Colors.white,
+                ),
+                Chip(
+                  avatar: CircleAvatar(
+                      backgroundColor: Colors.red,
+                      child: Text('O', style: TextStyle(color: Colors.white))),
+                  label: Text('Oreo'),
+                  backgroundColor: Colors.white,
+                ),
+                Chip(
+                  avatar: CircleAvatar(
+                      backgroundColor: Colors.greenAccent,
+                      child:
+                          Text('P', style: TextStyle(color: Colors.black45))),
+                  label: Text('Pie'),
+                  backgroundColor: Colors.white,
+                ),
+              ],
+            )
           ],
         ),
       ),
@@ -846,6 +949,36 @@ class ProfilePage extends StatelessWidget {
                   },
                   child: Text('Add Hashtag'),
                 ),
+                SizedBox(height: 20),
+                Wrap(
+                  spacing: 4.0, // gap between lines
+                  children: <Widget>[
+                    Chip(
+                      avatar: CircleAvatar(
+                          backgroundColor: Colors.orange,
+                          child:
+                              Text('C', style: TextStyle(color: Colors.white))),
+                      label: Text('Cupcake'),
+                      backgroundColor: Colors.white,
+                    ),
+                    Chip(
+                      avatar: CircleAvatar(
+                          backgroundColor: Colors.cyanAccent,
+                          child: Text('D',
+                              style: TextStyle(color: Colors.black45))),
+                      label: Text('Donut'),
+                      backgroundColor: Colors.white,
+                    ),
+                    Chip(
+                      avatar: CircleAvatar(
+                          backgroundColor: Colors.indigoAccent,
+                          child:
+                              Text('E', style: TextStyle(color: Colors.white))),
+                      label: Text('Eclair'),
+                      backgroundColor: Colors.white,
+                    ),
+                  ],
+                )
               ],
             ),
           ],
